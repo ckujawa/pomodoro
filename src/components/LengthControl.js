ecb44e5da8059d9e14c7879545d51f158622ca42
Chris Kujawa <ckujawa@chriskujawadevelopment.com>
2019-07-18 12:42:46 -0400

    added styled components

--- src/components/LengthControl.js
+++ src/components/LengthControl.js
@@ -2,28 +2,72 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import PropTypes from "prop-types";
+import styled from 'styled-components';
 import { faArrowUp, faArrowDown } from "@fortawesome/free-solid-svg-icons";
+
+const Control = styled.div`
+  margin: 0 20px;
+  padding: 10px;
+  width: 20%;
+  border: 3px solid var(--dark);
+  border-radius: 7px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const ControlWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin: 0;
+  padding: 0;
+
+`;
+
+const Button = styled.button`
+  line-height: 1.2em;
+  font-size: 1em;
+  font-weight: bold;
+  margin: 0 10px;
+  padding: 0 10px;
+  border: 3x ridge var(--dark);
+  background: var(--light);
+  border-radius: 5px;
+  box-shadow: 3px 3px 5px black;
+
+  &:hover{
+    transform: scale(1.1);
+    box-shadow: 4px 4px 7px black;
+  }
+
+  & > div{
+    font-size: 5em;
+  }
+`;
 
 const LengthControl = ({ compName, title, clickFunc, length }) => {
   return (
-    <div id={`${compName}-control`}>
+    <Control id={`${compName}-control`}>
       <div id={`${compName}-label`}>{title}</div>
-      <button
-        id={`${compName}-decrement`}
-        value="-"
-        onClick={() => clickFunc(compName, "-")}
-      >
-        <FontAwesomeIcon icon={faArrowDown} />
-      </button>
-      <div id={`${compName}-length`}>{length}</div>
-      <button
-        id={`${compName}-increment`}
-        value="+"
-        onClick={() => clickFunc(compName, "+")}
-      >
-        <FontAwesomeIcon icon={faArrowUp} />
-      </button>
-    </div>
+      <ControlWrapper>
+        <Button
+          id={`${compName}-decrement`}
+          value="-"
+          onClick={() => clickFunc(compName, "-")}
+        >
+          <FontAwesomeIcon icon={faArrowDown} />
+        </Button>
+        <div id={`${compName}-length`}>{length}</div>
+        <Button
+          id={`${compName}-increment`}
+          value="+"
+          onClick={() => clickFunc(compName, "+")}
+        >
+          <FontAwesomeIcon icon={faArrowUp} />
+        </Button>
+      </ControlWrapper>
+    </Control>
   );
 };
 
